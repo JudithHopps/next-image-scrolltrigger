8a772b5903eaf49ec7460ceca32463455638b3e7
JudithHopps <kookjeayon100@gmail.com>
2024-12-23 10:36:33 +0900

    feat : theme Provider

---
+++ src/types/next-themes.d.ts
@@ -0,0 +1,11 @@
+declare module "next-themes/dist/types" {
+  import { ReactNode } from "react";
+
+  export interface ThemeProviderProps {
+    children?: ReactNode;
+    attribute?: string;
+    defaultTheme?: string;
+    enableSystem?: boolean;
+    themes?: string[];
+  }
+}
