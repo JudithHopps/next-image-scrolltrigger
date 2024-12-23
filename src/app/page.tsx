c6ddb67be53aeecf776329feb5f505b719bc1419
JudithHopps <kookjeayon100@gmail.com>
2024-12-23 10:36:17 +0900

    feat : main page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,95 +1,136 @@
+"use client";
+
+import { useEffect } from "react";
+import Link from "next/link";
+import gsap from "gsap";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
+import { ReactLenis } from "lenis/react";
 import Image from "next/image";
-import styles from "./page.module.css";
+
+gsap.registerPlugin(ScrollTrigger);
 
 export default function Home() {
-  return (
-    <div className={styles.page}>
-      <main className={styles.main}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol>
-          <li>
-            Get started by editing <code>src/app/page.tsx</code>.
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+  useEffect(() => {
+    const ScrollTriggerSettings = {
+      trigger: ".main",
+      start: "top 25%",
+      toggleActions: "play reverse play reverse",
+    };
 
-        <div className={styles.ctas}>
-          <a
-            className={styles.primary}
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className={styles.logo}
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-            className={styles.secondary}
-          >
-            Read our docs
-          </a>
+    const leftXvalues = [-800, -900, -400];
+    const rightXvalues = [800, 900, 400];
+    const leftRotationValues = [-30, -20, -35];
+    const rightRotationValues = [30, 20, 35];
+    const yValues = [100, -150, -400];
+
+    gsap.utils.toArray<HTMLElement>(".row").forEach((row, index) => {
+      const cardLeft = row.querySelector<HTMLElement>(".card-left");
+      const cardRight = row.querySelector<HTMLElement>(".card-right");
+
+      if (cardLeft && cardRight) {
+        gsap.to(cardLeft, {
+          x: leftXvalues[index],
+          scrollTrigger: {
+            trigger: ".main",
+            start: "top center",
+            end: "150% bottom",
+            scrub: true,
+            onUpdate: (self) => {
+              const progress = self.progress;
+              cardLeft.style.transform = `translateX(${
+                progress * leftXvalues[index]
+              }px) 
+            translateY(${progress * yValues[index]}px) 
+            rotate(${progress * leftRotationValues[index]}deg)`;
+
+              cardRight.style.transform = `translateX(${
+                progress * rightXvalues[index]
+              }px) 
+            translateY(${progress * yValues[index]}px) 
+            rotate(${progress * rightRotationValues[index]}deg)`;
+            },
+          },
+        });
+      }
+    });
+
+    gsap.to(".logo", {
+      scale: 1,
+      duration: 0.5,
+      ease: "power1.out",
+      scrollTrigger: ScrollTriggerSettings,
+    });
+
+    gsap.to(".line p", {
+      y: 0,
+      stagger: 0.1,
+      duration: 0.5,
+      ease: "power1.out",
+      scrollTrigger: ScrollTriggerSettings,
+    });
+
+    gsap.to("button", {
+      y: 0,
+      opacity: 1,
+      stagger: 0.25,
+      duration: 0.5,
+      ease: "power1.out",
+      scrollTrigger: ScrollTriggerSettings,
+    });
+  }, []);
+
+  const ImageRows = () => {
+    return Array.from({ length: 3 }, (_, i) => (
+      <div className="row" key={i}>
+        <div className="card card-left">
+          <Image src={`/img-${2 * i + 1}.png`} alt="img" fill />
         </div>
-      </main>
-      <footer className={styles.footer}>
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
+        <div className="card card-right">
+          <Image src={`/img-${2 * i + 2}.png`} alt="img" fill />
+        </div>
+      </div>
+    ));
+  };
+
+  return (
+    <ReactLenis root>
+      <section className="hero">
+        <div className="img">
+          <Image src="/pro.png" alt="logo" fill />
+        </div>
+      </section>
+
+      <section className="main">
+        <div className="main-content">
+          <div className="logo">
+            <Image src="/file.svg" alt="logo" fill />
+          </div>
+
+          <div className="copy">
+            <div className="line">
+              <p>Delve into coding without clutter.</p>
+            </div>
+            <div className="line">
+              <p>One subscription. Endless web design.</p>
+            </div>
+            <div className="line">
+              <p>Take the fast lane to mastery.</p>
+            </div>
+          </div>
+
+          <div className="btn">
+            <button>Get Pro</button>
+          </div>
+        </div>
+
+        <ImageRows />
+      </section>
+
+      <section className="footer">
+        <Link href="codegrid.gumroad.com/l/codegridpro">
+          Link in description
+        </Link>
+      </section>
+    </ReactLenis>
   );
 }
